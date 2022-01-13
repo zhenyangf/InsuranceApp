3e7f66e4226261686b470504ba0993b2ad34fea6
zhenyangf <zhenyang.feng@stud.vgtu.lt>
2022-01-13 21:53:08 +0200

    ideta lubos kaino skaiciavimas

--- app/src/main/java/com/example/insuranceapp/Butas.java
+++ app/src/main/java/com/example/insuranceapp/Butas.java
@@ -2,27 +2,39 @@ package com.example.insuranceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Butas extends AppCompatActivity {
     EditText luboMetrai;
+    TextView lubosKaina;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_butas);
     }
 
-    public void lubosSkaiciavimas(){
+    public void lubosSkaiciavimas() {
         luboMetrai = findViewById(R.id.lubosMetrai);
         String metrai = luboMetrai.getText().toString();
         int value = Integer.parseInt(metrai);
         int pirmasAts;
         int antrasAts;
-        pirmasAts = value*45;
-        antrasAts = value*90;
+        pirmasAts = value * 45;
+        antrasAts = value * 90;
+        TextView kaina = (TextView)findViewById(R.id.luboskainas);
+        kaina.setText("Kainuos tarp " + pirmasAts + " - " +antrasAts);
+    }
+    @SuppressLint("SetTextI18n")
+    public void calculate(View view){
+        lubosSkaiciavimas();
 
     }
+
+
 
 
 }
